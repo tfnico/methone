f6e958bb757595b58d6af53b2f322ddbed20b078
Thomas Ferris Nicolaisen <tfnico@gmail.com>
2012-07-13 22:49:08 +0200

    Methone knows its name
    toString returns 'methone'

---
+++ src/main/java/com/tfnico/examples/building/methone/Methone.java
@@ -0,0 +1,9 @@
+package com.tfnico.examples.building.methone;
+
+public class Methone {
+
+	@Override
+	public String toString() {
+		return "methone";
+	}
+}
---
+++ src/test/java/com/tfnico/examples/building/methone/MethoneTest.java
@@ -0,0 +1,15 @@
+package com.tfnico.examples.building.methone;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MethoneTest {
+
+	@Test
+	public void test() {
+		Methone methone = new Methone();
+		assertEquals("methone",methone.toString());
+	}
+
+}
